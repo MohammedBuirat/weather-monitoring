63a63b851e1dae269754f9f99af3d596c5b5117c
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-27 13:26:21 +0300

    Implement Bots

---
+++ weeather/Entities/Bot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather.Entities
+{
+    internal class Bot
+    {
+        public string Name { get; set; }
+        public Condition Condition { get; set; }
+        public string Message { get; set; }
+        public bool Enabled { get; set; }
+        bool ConditionAboveTheThreshhold { get; set; }
+
+        public Bot(string botName, Condition condition, string message, bool enabled, bool conditionAboveTheThreshhold)
+        {
+            Name = botName;
+            Condition = condition;
+            Message = message;
+            Enabled = enabled;
+            ConditionAboveTheThreshhold = conditionAboveTheThreshhold;
+        }
+
+        public bool IsBotActivated(decimal temperature, decimal humidity)
+        {
+            bool valid = false;
+            if (Condition.Type == ConditionType.Temperature)
+            {
+                valid = TriggerCondition(temperature);
+            }
+            else if (Condition.Type == ConditionType.Humidity)
+            {
+                valid = TriggerCondition(humidity);
+            }
+            return valid;
+        }
+
+        private bool TriggerCondition(decimal value)
+        {
+            bool isMoreThanThreshold = ConditionAboveTheThreshhold && (value > Condition.Threshold);
+            bool lessThanThreshold = !ConditionAboveTheThreshhold && (value < Condition.Threshold);
+            return isMoreThanThreshold || lessThanThreshold;
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine($"{Name} activated!");
+            Console.WriteLine($"{Name}:\" {Message} \"");
+        }
+    }
+}
---
+++ weeather/Entities/Condition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather.Entities
+{
+    internal class Condition
+    {
+        public ConditionType Type { get; set; }
+        public decimal Threshold { get; set; }
+
+        public Condition(ConditionType type, decimal threshold)
+        {
+            Type = type;
+            Threshold = threshold;
+        }
+    }
+}
