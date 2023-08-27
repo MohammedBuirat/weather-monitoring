9c0bb8f544005201529b8b47e24ac8b0cbd92a38
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-27 08:47:09 +0300

    Creat Weather data class

---
+++ weeather/Entities/WeatherData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weeather.Entities
+{
+    internal class WeatherData
+    {
+        public string Location { get; set; }
+        public decimal Temparture { get; set; }
+        public decimal Humidity { get; set; }
+        
+        public WeatherData() { }
+
+        public WeatherData(string location,  decimal temparture, decimal humidity)
+        {
+            Location = location;
+            Temparture = temparture;
+            Humidity = humidity;
+        }
+
+    }
+}
